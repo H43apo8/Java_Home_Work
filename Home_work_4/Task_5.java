817a45454952f027e8107af208a08d00b35c6eee
H43apo8 <H43apo8@yandex.ru>
2023-04-05 13:22:22 +0300

    Task_5: "Логически правильная строка"

---
+++ Home_work_4/Task_5.java
@@ -0,0 +1,41 @@
+//Task_5
+//Дана строка содержащая только символы '(', ')', '{', '}', '[' и ']', определите,
+// является ли входная строка логически правильной.
+// Входная строка логически правильная, если:
+// 1) Открытые скобки должны быть закрыты скобками того же типа.
+// 2) Открытые скобки должны быть закрыты в правильном порядке. Каждая закрывающая скобка имеет соответствующую
+// открытую скобку того же типа.
+// ()[] = true
+// () = true
+// {[()]} = true
+// ()() = true
+// )()( = false
+
+import java.util.Deque;
+import java.util.LinkedList;
+public class Task_5 {
+    public static void main(String[] args) {
+        String input = "{[()]}";
+        System.out.println(validate(input));
+    }
+
+    public static boolean validate(String input) {
+        Deque<Character> stack = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
